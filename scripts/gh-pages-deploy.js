138651598cc688cc37d3bf23eb00dffb9e44f8e9
mxswat <massimocacciapaglia.13@gmail.com>
2020-04-21 02:11:22 +0200

    changed deploy script

--- scripts/gh-pages-deploy.js
+++ scripts/gh-pages-deploy.js
@@ -7,13 +7,11 @@ const fs = require("fs");
         console.log("Building...");
         await execa("npm", ["run", "build"]);
         // Understand if it's dist or build folder
-        const folderName = fs.existsSync("dist") ? "dist" : "build";
-        await execa("git", ["--work-tree", folderName, "add", "--all"]);
-        await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
+        await execa("git", ["--work-tree", 'dist', "add", "--all"]);
+        await execa("git", ["--work-tree", 'dist', "commit", "-m", "gh-pages"]);
         console.log("Pushing to gh-pages...");
         await execa("git", ["push", "origin", "HEAD:gh-pages", "--force"]);
-        // await execa("rm", ["-r", folderName]);
-        await execa("del", ["-r", folderName]); // Windows only
+        await execa("del", ["-r", 'dist']); // Windows only
         await execa("git", ["checkout", "-f", "master"]);
         await execa("git", ["branch", "-D", "gh-pages"]);
         console.log("Successfully deployed");

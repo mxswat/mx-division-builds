878d89b61d3c8bc25ba404d78e26c50efc8d023d
mxswat <massimocacciapaglia.13@gmail.com>
2020-04-22 15:37:44 +0200

    Changed headers for weapons

--- src/utils/agGridDefaults.js
+++ src/utils/agGridDefaults.js
@@ -16,21 +16,21 @@ const gearHeaders =  [
 ];
 
 const weaponsHeaders = [
-    { headerName: 'Variant', field: 'Variant', ...columnDef },
+    { headerName: 'Weapon Type', field: 'Weapon Type', ...columnDef },
     { headerName: 'Weapon', field: 'Weapon', ...columnDef },
     // { headerName: 'Slot', field: 'Slot', ...columnDef }, hidden, not really needed for now
     { headerName: 'Type', field: 'Type', ...columnDef },
-    { headerName: 'Weapon Type', field: 'Weapon Type', ...columnDef },
     { headerName: 'Base Damage', field: 'Base Damage', ...columnDef },
     { headerName: 'Mag Size', field: 'Mag Size', ...columnDef },
     { headerName: 'RPM', field: 'RPM', ...columnDef },
     { headerName: 'Reload Speed (ms)', field: 'Reload Speed (ms)', ...columnDef },
     { headerName: 'Optimal Range', field: 'Optimal Range', ...columnDef },
+    { headerName: 'Variant', field: 'Variant', ...columnDef },
     { headerName: 'Core 1', field: 'Core 1', ...columnDef },
     { headerName: 'Core 1 Max', field: 'Core 1 Max', ...columnDef },
     { headerName: 'Core 2', field: 'Core 2', ...columnDef },
     { headerName: 'Core 2 Max', field: 'Core 2 Max', ...columnDef },
-    { headerName: 'Attr. 1', field: 'Attr. 1', ...columnDef },
+    { headerName: 'Attribute 1', field: 'Attribute 1', ...columnDef },
     { headerName: 'Talent', field: 'Talent', ...columnDef },
     { headerName: 'Optics', field: 'Optics', ...columnDef },
     { headerName: 'Under Barrel', field: 'Under Barrel', ...columnDef },

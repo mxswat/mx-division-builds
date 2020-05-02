17084d7c2a827814401f16ff3f4765de02f1a626
mxswat <massimocacciapaglia.13@gmail.com>
2020-05-02 14:12:25 +0200

    Formula in code

--- src/utils/statsCalculator.js
+++ src/utils/statsCalculator.js
@@ -1,3 +1,17 @@
-// baseDMG*(AWD+WD+Weapon Increases talents)*(CHD + HSD) * (Total increases talents from Gear) * AmpN * AmpN
-// AmpN is an "Amplifies" talent or DtA, DtOOC, DtH
-// Check notes
+/**
+ * ----- Variables -----
+ * baseDMG = Weapon damage
+ * AWD = All red cores of a build
+ * WD = Specific weapon damage
+ * Weapon Increases talents = Optimist and similiar stuff
+ * CHD = Critical Hit Damage
+ * HSD = Headshot damage
+ * Total additive talents from gear = Additive stuff like Focus and vigilance
+ * AmpN = Every amplifier talent or attribute = DTA, DttOOC, DTA on the weapon, DttOOC on the weapon
+ * 
+ * ----- Formula -----
+ * CRIT HS = baseDMG*(AWD + WD + Weapon Increases talents ) * (CHD + HSD) * (Total additive talents from gear) * AmpN * AmpN
+ * CRIT BODY = baseDMG*(AWD + WD + Weapon Increases talents ) * (CHD) * (Total additive talents from gear) * AmpN * AmpN
+ * HS = baseDMG*(AWD + WD + Weapon Increases talents ) * (HSD) * (Total additive talents from gear) * AmpN * AmpN
+ * BODY = baseDMG*(AWD + WD + Weapon Increases talents ) * (Total additive talents from gear) * AmpN * AmpN
+ */

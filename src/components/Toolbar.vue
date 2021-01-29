56c586a20da46ddaab0892ace5bacea45868b66e
Massimo 'Mx' Cacciapaglia <massimocacciapaglia.13@gmail.com>
2021-01-29 18:19:08 +0000

    Mobile menu Fix

--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -1,28 +1,42 @@
 <template>
   <div class="toolbar">
-    <span>The Division 2 Builds tool</span>
+    <span class="app-title">The Division 2 Builds tool</span>
+    <input
+      class="build-name"
+      type="text"
+      placeholder="Your build Name"
+      name="Build Name"
+    />
     <div class="spacer"></div>
+    <a @click="showMobileMenu = !showMobileMenu" class="menu-btt arrow-down">Menu</a>
+    <div class="toolbar-inner" :class="{ 'showOnMobile': showMobileMenu }">
+      <button @click="saveAndShare()">Save & Share</button>
+      <button @click="screenshot()" id="screenshotBTT">Save Screenshot</button>
+      <button @click="clear()">Clear</button>
+      <button @click="newBuild()">New Build</button>
+      <button style="color: #f7d07c" @click="issues()">Issues or bug?</button>
+    </div>
     <!-- 
       Bugged TODO: Fix me
     <button @click="history.back()">Undo</button>
     <button @click="history.forward()">Redo</button> 
     -->
-    <button @click="saveAndShare()">Save & Share</button>
-    <button @click="screenshot()" id="screenshotBTT">Save Screenshot</button>
-    <button @click="clear()">Clear</button>
-    <button @click="newBuild()">New Build</button>
-    <button style="color: #f7d07c" @click="issues()">Issues or bug?</button>
   </div>
 </template>
 
 <script>
-import { openSaveShareModal, openIssueModal, openScreenshotModal } from "../utils/modalService";
+import {
+  openSaveShareModal,
+  openIssueModal,
+  openScreenshotModal,
+} from "../utils/modalService";
 
 export default {
   name: "Toolbar",
   data() {
     return {
       history,
+      showMobileMenu: false
     };
   },
   created() {
@@ -56,15 +70,84 @@ export default {
 <style lang="scss" scoped>
 .toolbar {
   display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
 }
 .spacer {
   flex: 1 1;
 }
 
-button {
-  margin-left: 8px;
+.toolbar-inner button {
+  margin-right: 8px;
   color: white;
   border: 1px solid white;
   background: transparent;
+  min-height: 32px;
+}
+
+.app-title {
+  margin-left: 8px;
+  margin-right: 8px;
+  height: 31px;
+  line-height: 31px;
+}
+
+.build-name {
+  $height: 31px;
+  height: $height;
+  line-height: $height;
+  padding: 0px;
+  margin-left: 8px;
+  // background: rgba(0, 0, 0, 0.3);
+  background: transparent;
+  border: 0px;
+  border-bottom: 1px solid white;
+  font-size: 16px;
+  color: white;
+  font-family: "Titillium Web", sans-serif;
+  font-weight: 400;
+  min-width: 275px;
+}
+
+.menu-btt {
+  display: none;
+  height: 31px;
+  line-height: 31px;
+  color: white;
+  padding-right: 24px;
+  background-color: transparent;
+  border: 0px;
+  background-position: right;
+  border-bottom: 1px solid white;
+  margin-top: 8px;
+  margin-bottom: 4px;
+  margin-right: 8px;
+  margin-left: 8px;
+  width: 100%;
+}
+
+@media only screen and (max-width: 964px) {
+  button {
+    margin-top: 8px;
+  }
+}
+
+// mobile switch to menu W/ button
+@media only screen and (max-width: 506px) {
+  .menu-btt {
+    display: flex;
+  }
+  .toolbar-inner {
+    display: none;
+    &.showOnMobile {
+      display: flex;
+      flex-direction: column;
+      width: 100%;
+      button {
+        margin-right: 0;
+        width: 100%;
+      }
+    }
+  }
 }
 </style>

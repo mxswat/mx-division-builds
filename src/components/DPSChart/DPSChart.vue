14a95c969ee1b1eb97fbe4cebf9f656d7b73721a
Massimo 'Mx' Cacciapaglia <massimocacciapaglia.13@gmail.com>
2021-01-31 14:53:09 +0000

    Added color blind traces

--- src/components/DPSChart/DPSChart.vue
+++ src/components/DPSChart/DPSChart.vue
@@ -1,15 +1,23 @@
 <template>
   <div>
     <span class="section-title"> DPS Visualization </span>
-    <span> READ ME! Click on the trace on the legend to hide or show it! </span>
+    <div class="toggles">
+      <Toggle
+        style="max-width: 126px"
+        @input="updatedToggle()"
+        v-model="isColorblind"
+        :label="'Color blind?'"
+      ></Toggle>
+    </div>
     <div id="chart-test"></div>
+    <span> Click on the trace on the legend to hide or show it! </span>
   </div>
 </template>
 
 <script>
 import Plotly from "plotly.js-dist";
 import DPSChartCore from "../../utils/DPSChartCore";
-
+import Toggle from "../generic/Toggle";
 const DEFAULT_PLOT = {
   margin: { t: 25, b: 25, l: 50, r: 50 },
   plot_bgcolor: "#1a1e24",
@@ -30,29 +38,47 @@ const DEFAULT_PLOT = {
   legend: { orientation: "h" },
 };
 const DEFAULT_PLOT_2 = { showSendToCloud: true, responsive: true };
+const dashStyles = ["solid", "dashdot", "dot"];
+const colors = ["#E69F00", "#56B4E9", "#009E73"];
 export default {
   name: "DPSChart",
+  components: { Toggle },
   data() {
     return {
       target: null,
+      isColorblind: false,
+      cacheWeapons: [],
     };
   },
   created() {
     DPSChartCore.subscribeToCombinedWeaponUpdates().subscribe((weapons) => {
-      const weaponTraces = [];
-      for (let i = 0; i < weapons.length; i++) {
-        const weapon = weapons[i];
-        if (weapon) {
-          weaponTraces.push(weapon);
-        }
-      }
-
-      Plotly.react(this.target, weaponTraces, DEFAULT_PLOT, DEFAULT_PLOT_2);
+      this.cacheWeapons = weapons;
+      this.updateChart(weapons);
     });
   },
   mounted() {
     this.target = document.getElementById("chart-test");
     Plotly.newPlot(this.target, [], DEFAULT_PLOT, DEFAULT_PLOT_2);
+  },
+  methods: {
+    updateChart(weapons) {
+      const weaponTraces = [];
+      for (let i = 0; i < weapons.length; i++) {
+        const weapon = weapons[i];
+        if (weapon) {
+          weapon.line = {
+            dash: this.isColorblind ? dashStyles[i] : undefined,
+            color: colors[i],
+          };
+          weaponTraces.push(weapon);
+        }
+      }
+
+      Plotly.react(this.target, weaponTraces, DEFAULT_PLOT, DEFAULT_PLOT_2);
+    },
+    updatedToggle() {
+      this.updateChart(this.cacheWeapons);
+    },
   },
 };
 </script>
@@ -60,5 +86,9 @@ export default {
 <style lang="scss">
 div#chart-test {
   min-height: 600px;
+}
+
+.toggles {
+  display: flex;
 }
 </style>

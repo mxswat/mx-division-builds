d4e2fe6e705aae2e36ffb3aa5cddd82b3180e0f2
Massimo 'Mx' Cacciapaglia <massimocacciapaglia.13@gmail.com>
2021-02-06 17:29:09 +0000

    Improved reload timer

--- src/utils/DPSChartCore.js
+++ src/utils/DPSChartCore.js
@@ -22,9 +22,7 @@ class DPSChartCoreService {
     addWeaponTrace(category, weaponStats) {
         let timeToEmptyMagazine = (weaponStats.totalMagSize / (weaponStats.rpm / 60)) * 1000;
         const dataPointsCount = Math.round(60000 / (timeToEmptyMagazine + weaponStats.reloadSpeed));
-        // const dataPoints = new Array(dataPointsCount + 1);
         const dataPointsToRenderCount = dataPointsCount + 1;
-        let isReloadingTime = false
         let damageDelta = 0
         let timeDelta = 0
         const timeAxis = new Array(dataPointsToRenderCount);
@@ -32,16 +30,15 @@ class DPSChartCoreService {
         timeAxis[0] = 0;
         damageAxis[0] = 0;
         for (let i = 1; i < dataPointsToRenderCount; i++) {
+            const isReloadingTime = !(i % 2); // not (i % 2) becuse the first iteration is always not reloading 
             let damage = !isReloadingTime ? Number(weaponStats.dmgToOutOfCoverArmoredPerMag) + damageDelta : damageDelta;
             let time = isReloadingTime ? timeDelta + weaponStats.reloadSpeed : timeDelta + timeToEmptyMagazine;
             timeAxis[i] = time / 1000;
             damageAxis[i] = this.roundValue(damage);
             timeDelta = time;
             damageDelta = damage;
-            isReloadingTime = !isReloadingTime;
         }
 
-        // ${(this.toggleHSD && " HSD") || ""}${(this.toggleCHD && " CHD") || ""}
         this._subjects[category].next({
             name: `${category}: ${weaponStats.weaponName}`,
             x: timeAxis,

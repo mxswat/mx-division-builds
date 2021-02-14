553b517349320bec6a00b69393d99bf1c29dd2c4
Massimo 'Mx' Cacciapaglia <massimocacciapaglia.13@gmail.com>
2021-02-14 22:57:43 +0000

    a bit of UI fixes

--- src/components/TTK/TimeToKill.vue
+++ src/components/TTK/TimeToKill.vue
@@ -1,11 +1,9 @@
 <template>
   <div class="ttk-ui">
-    <span class="section-title">
-      Time To Kill / Bullets to Kill
-    </span>
+    <span class="section-title"> Time To Kill / Bullets to Kill </span>
     <template v-for="(tablesData, idx) in data">
       <div class="weapon-tables-container" v-bind:key="idx" v-if="tablesData">
-        <span>{{ tablesData.weaponName }}</span>
+        <span class="ttk-weapon-name">{{ tablesData.weaponName }}</span>
         <div class="ttk-tables">
           <ResponsiveTable
             v-for="(tables, idx) in tablesData.tables"
@@ -57,5 +55,12 @@ export default {
   display: grid;
   grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
   gap: 20px;
+}
+
+span.ttk-weapon-name {
+  font-weight: 800;
+  font-size: 18px;
+  margin-top: 16px;
+  display: block;
 }
 </style>

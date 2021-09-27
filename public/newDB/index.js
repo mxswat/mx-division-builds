c192002072cba77e8b4cda5b45d304c62617e182
mxswat <massimocacciapaglia.13@gmail.com>
2021-09-27 01:51:24 +0100

    perfect json generation

--- public/newDB/index.js
+++ public/newDB/index.js
@@ -3,7 +3,11 @@ const weapons = require('./Weapons - Weapons.json')
 const newWeapons = []
 
 function strndrdize(string) {
-  return string.replace(/[^A-Z0-9]+/ig, "_").toLowerCase()
+  if (Number.isInteger(string)) {
+    return string
+  }
+  // Remove special chars, and the replace spaces with _
+  return string.replace(/[^a-zA-Z ]/g, "").replace(/[^A-Z0-9]+/ig, "_").toLowerCase()
 }
 
 for (let i = 0; i < weapons.length; i++) {
@@ -21,12 +25,20 @@ for (let i = 0; i < weapons.length; i++) {
       `reload_speed:${strndrdize(weapon['Reload Speed (ms)'])}`,
       `hsd:${strndrdize(weapon['HSD'])}`,
       `core:${strndrdize(weapon['Core 1'])}`,
-      weapon['Core 2'] && `core:${strndrdize(weapon['Core 2'])}`,
+      ...(weapon['Core 2'] ? [`core:${strndrdize(weapon['Core 2'])}`] : []),
       `attribute:${strndrdize(weapon['Attribute 1'])}`,
       `talent:${strndrdize(weapon['Talent'])}`,
+      ...(weapon['Optics'].split(' / ').map(x => `mod_optics:${strndrdize(x)}`)),
+      ...(weapon['Under Barrel'].split(' / ').map(x => `mod_underbarrel:${strndrdize(x)}`)),
+      ...(weapon['Magazine'].split(' / ').map(x => `mod_magazine:${strndrdize(x)}`)),
+      ...(weapon['Muzzle'].split(' / ').map(x => `mod_muzzle:${strndrdize(x)}`)),
     ],
     tags: [
-
+      weapon.Quality == 'Exotic' 
+        ? 'is_exotic'
+        :  weapon.Quality == 'Named' 
+            ? 'is_named'
+            : 'is_high_end'
     ]
   }
   newWeapons.push(newWeapon)
